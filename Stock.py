b07dbe7f1a16ecbc57397aa61ae5c3554121b583
yamtimor <yamtimor@gmail.com>
2023-05-06 13:46:46 +0300

    try to use the class

--- Stock.py
+++ Stock.py
@@ -1,6 +1,8 @@
-import yfinance
+import yfinance as yf
 
 class Stock(yf.Ticker):
     def __init__(self, symbol):
         super().__init__(symbol)
 
+stock = Stock("MSFT")
+actions = stock.actions
